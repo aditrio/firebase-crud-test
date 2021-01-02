044b42ece0724b7dc23af4a994711e2123ac981f
aditrio <30noadi@gmail.com>
2021-01-02 22:07:29 +0700

    firebase conn

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+
+
+  console.log("test");
+  
+  var firebaseConfig = {
+    apiKey: "AIzaSyCGM52QuuiH-s6-6nd8AI8kXV3dMZgy57U",
+    authDomain: "uas-sbd.firebaseapp.com",
+    projectId: "uas-sbd",
+    storageBucket: "uas-sbd.appspot.com",
+    messagingSenderId: "108308801737",
+    appId: "1:108308801737:web:2bfbd5a83b26036915c518",
+    measurementId: "G-9LY6KW99NF"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
